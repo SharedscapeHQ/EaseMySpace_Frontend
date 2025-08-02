5140238fea1c5d457f181692f8b167b02f9d45e2
EaseMySpace <hello@easemyspace.in>
2025-08-02 12:44:54 +0530

    all api conflicts

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
 import toast from "react-hot-toast";
-import { registerUser } from "../API/authAPI";
+import { registerUser } from "../api/authAPI";
 
 function Register() {
   const [form, setForm] = useState({
