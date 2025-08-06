b3d80c500ef6f3ea7b9df668dd1623b95370ae70
EaseMySpace <hello@easemyspace.in>
2025-08-06 17:33:58 +0530

    top location section UI

--- src/components/LandingSectionComp/TopLocation.jsx
+++ src/components/LandingSectionComp/TopLocation.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import location_vid from "/location-vid.gif";
 import { getAllLocations } from "../../api/adminApi";
 
 function TopLocation() {
@@ -32,7 +31,7 @@ function TopLocation() {
   return (
     <section
       style={{ fontFamily: "para_font" }}
-      className="w-full lg:pb-10  bg-zinc-50"
+      className="w-full lg:pb-10 bg-zinc-50"
     >
       <div className="max-w-7xl lg:px-10 px-3 mx-auto">
         <h2
@@ -42,39 +41,29 @@ function TopLocation() {
           Top Locations
         </h2>
 
-        <div className="flex  overflow-x-auto gap-5 scrollbar-hide">
+        <div className="flex overflow-x-auto gap-5 scrollbar-hide">
           {loading
             ? Array.from({ length: 5 }).map((_, i) => (
                 <div
                   key={i}
-                  className=" group w-[170px] h-[170px] sm:min-w-0 flex-shrink-0 bg-zinc-200 animate-pulse rounded-xl"
+                  className="group w-[170px] h-[170px] sm:min-w-0 flex-shrink-0 bg-zinc-200 animate-pulse rounded-xl"
                 />
               ))
             : locations.map((loc, i) => (
                 <div
-  key={i}
-  onClick={() => handleLocationClick(loc.name)}
-  className="group w-[165px] sm:min-w-0 flex-shrink-0 bg-white-900 bg-gradient-to-r from-white to-[#cbe0fd] rounded-xl p-4 shadow-sm transition-all flex flex-col items-center text-center cursor-pointer border-2 border-zinc-200"
->
-  {loc.image ? (
-  <img
-    src={loc.image}
-    alt={loc.name}
-    className="w-32 h-32 object-cover  rounded"
-  />
-) : (
-  <img
-    src={location_vid}
-    alt="Default location"
-    className="w-28 h-28 object-cover mb-2 rounded"
-  />
-)}
-
-  <p className="text-sm capitalize text-zinc-800 font-medium min-h-[1.5rem] transition-all duration-200 group-hover:scale-125 group-hover:text-zinc-800">
-    {loc.name}
-  </p>
-</div>
-
+                  key={i}
+                  onClick={() => handleLocationClick(loc.name)}
+                  className="group w-[140px] h-[140px] sm:min-w-0 flex-shrink-0 bg-gradient-to-br from-white to-blue-100 rounded-xl p-3 transition-all flex flex-col items-center justify-center text-center cursor-pointer border border-zinc-200"
+                >
+                  <img
+                    src={loc.image}
+                    alt={loc.name}
+                    className="w-32 h-32 object-cover rounded-lg transition-transform duration-300"
+                  />
+                  <p className=" pb-4 text-[13px] capitalize text-zinc-800 font-medium lg:group-hover:scale-125 transition-all duration-200">
+                    {loc.name}
+                  </p>
+                </div>
               ))}
         </div>
       </div>
