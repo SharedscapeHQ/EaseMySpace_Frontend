5482605938d69e68f815b20196d3688afb26f788
EaseMySpace <hello@easemyspace.in>
2025-08-13 14:09:51 +0530

    carrers page hide

--- src/components/AboutUsSectionComp/CareersPageComp/jobsData.js
+++ src/components/AboutUsSectionComp/CareersPageComp/jobsData.js
@@ -1,28 +1,28 @@
 const jobs = [
-  {
-    id: 1,
-    dept: "Engineering",
-    role: "Frontend Developer",
-    location: "Remote",
-    description:
-      "Work on building beautiful, fast, and responsive UI using React. Collaborate closely with backend engineers and designers.",
-  },
-  {
-    id: 2,
-    dept: "Marketing",
-    role: "Content Strategist",
-    location: "Mumbai",
-    description:
-      "Plan, create, and manage impactful content across channels. Must have experience in brand storytelling and SEO.",
-  },
-  {
-    id: 3,
-    dept: "Design",
-    role: "UI/UX Designer",
-    location: "Mumbai",
-    description:
-      "Design intuitive and engaging user experiences for web and mobile applications. Strong Figma skills required.",
-  },
+//   {
+//     id: 1,
+//     dept: "Engineering",
+//     role: "Frontend Developer",
+//     location: "Remote",
+//     description:
+//       "Work on building beautiful, fast, and responsive UI using React. Collaborate closely with backend engineers and designers.",
+//   },
+//   {
+//     id: 2,
+//     dept: "Marketing",
+//     role: "Content Strategist",
+//     location: "Mumbai",
+//     description:
+//       "Plan, create, and manage impactful content across channels. Must have experience in brand storytelling and SEO.",
+//   },
+//   {
+//     id: 3,
+//     dept: "Design",
+//     role: "UI/UX Designer",
+//     location: "Mumbai",
+//     description:
+//       "Design intuitive and engaging user experiences for web and mobile applications. Strong Figma skills required.",
+//   },
 ];
 
 export default jobs;
